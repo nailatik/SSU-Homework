933ab6e2b72af03d6294074d465b32a9895ab35f
Niall Tugushev <nail.tugushev30@yandex.ru>
2024-03-31 21:15:40 +0300

    Update 1.1: Two more versions of sorting have been implemented: selection and merge.

--- counting_sort.cpp
+++ counting_sort.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-#include <string>
 #include <fstream>
 #include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -22,22 +23,52 @@ int getYear(const string& dateString) {
 
 void countingSort(vector<Person>& persons, vector<int>& birthYears) {
     vector<Person> sortedPersons(persons.size());
-    vector<int> count(10000, 0);
+    vector<int> count(10000, 0); // Создается вспомогательный массив count, где индекс элемента соответствует значению года рождения, а значения - количество элементов с этим годом рождения.
 
+    // Подсчитываем количество людей с каждым конкретным годом рождения.
     for (int i = 0; i < birthYears.size(); i++) {
         count[birthYears[i]]++;
     }
 
+    // Накопление значений в массиве count. Определяем конечные позиции каждого уникального года рождения.
     for (int i = 1; i < count.size(); i++) {
         count[i] += count[i - 1];
     }
 
+    // Находится правильная позиция для человека, используя значения из массива count. Затем уменьшаем на единицу, обновляя тем самым позиции для следующего элемента.
     for (int i = persons.size() - 1; i >= 0; i--) {
         sortedPersons[count[birthYears[i]] - 1] = persons[i];
         count[birthYears[i]]--;
     }
 
     persons = sortedPersons;
+}
+
+void mergeSort(vector<Person>& persons) {
+    sort(persons.begin(), persons.end(), [](const Person& a, const Person& b) { //Разделяем на две части.
+        if (a.wage == b.wage) {
+            if (getYear(a.birthDate) == getYear(b.birthDate)) {
+                return a.exp < b.exp;  // Если зарплаты равны, переходим дальше.
+            }
+            return getYear(a.birthDate) < getYear(b.birthDate); // Сортируем по дате рождения.
+        }
+        return a.wage > b.wage; // Сортируем по зарплате.
+    });
+}
+
+void selectionSort(vector<Person>& persons) {
+    int n = persons.size();
+    for (int i = 0; i < n - 1; i++) { // Проходим по всем элементам вектора persons за исключением последнего элемента.
+        int cur = i;
+        for (int j = i + 1; j < n; j++) {
+            if (persons[j].wage > persons[cur].wage ||
+                (persons[j].wage == persons[cur].wage && getYear(persons[j].birthDate) < getYear(persons[cur].birthDate)) ||
+                (persons[j].wage == persons[cur].wage && getYear(persons[j].birthDate) == getYear(persons[cur].birthDate) && persons[j].exp < persons[cur].exp)) {
+                cur = j; // Обновляем индекс cur, если подходит нашему условию.
+            }
+        }
+        swap(persons[i], persons[cur]); // Повторяем процесс для следующего индекса i, уменьшая область поиска на 1 элемент.
+    }
 }
 
 int main() {
@@ -68,24 +99,33 @@ int main() {
     file.close();
 
     int numSort;
-    cout << "Hello! You can select the sorting type: " << endl;
-    cout << "1. Counting sotr \n" << "2. Diff sort\n" << "3. Diff sort\n" << "What type of sorting do you choose? --> ";
+    cout << "Hello! You can select the sorting type:\n1. Counting sort\n2. Merge sort (wage, then year)\n3. Selection sort (wage, then year, then experience)\nWhat type of sorting do you choose? --> ";
     cin >> numSort;
 
-    switch (numSort){
-        case 1: countingSort(persons, birthYears);
+    switch (numSort) {
+        case 1: 
+            countingSort(persons, birthYears);
+            break;
+        case 2:
+            mergeSort(persons);
+            break;
+        case 3:
+            selectionSort(persons);
+            break;
+        default:
+            cout << "Invalid sorting type selected.\n";
+            return 0;
     }
 
     ofstream outFile("output.txt");
-
     for (const Person& inf : persons) {
-        outFile << "Name: " << inf.name << endl;
-        outFile << "Rank: " << inf.rank << endl;
-        outFile << "BirthDate: " << inf.birthDate << endl;
-        outFile << "Experience: " << inf.exp << endl;
-        outFile << "Wage: " << inf.wage << endl;
-        outFile << endl;
+        outFile << "Name: " << inf.name << '\n';
+        outFile << "Rank: " << inf.rank << '\n';
+        outFile << "BirthDate: " << inf.birthDate << '\n';
+        outFile << "Experience: " << inf.exp << '\n';
+        outFile << "Wage: " << inf.wage << '\n';
     }
 
     return 0;
 }
+
